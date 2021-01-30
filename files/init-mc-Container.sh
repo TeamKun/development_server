12e25b6f8d07da899d3db4ae49b1e663ffbbe315
cwthdyk <chiwata.hide@gmail.com>
2021-01-31 02:36:34 +0900

    コンテナがないときでも起動できるように

--- files/init-mc-Container.sh
+++ files/init-mc-Container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-HOST_PORT=$(docker ps --format '{{.Ports}}' | awk -F'[:-]' '{print $2}' | sort -r | head -1 | awk '{nextport = $1+1} {print nextport}')
 CONTAINER_NAME=$1
 IPADDRESS=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
+
+HOST_PORT=$(docker ps --format '{{.Ports}}' | awk -F'[:-]' '{print $2}' | sort -r | head -1 | awk '{nextport = $1+1} {print nextport}')
+if [ ! -n "$TEST" ]
+then 
+    HOST_PORT=25565
+fi
 
 docker ps -a -f name="^${CONTAINER_NAME}$" --format '{{.Names}}' | grep ${CONTAINER_NAME}  > /dev/null
 if [ "$?" = "0" ]; then
